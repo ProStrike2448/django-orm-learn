4e6fb8bf6ef87badc01c85a2f4bc09fc26660db5
ProStrike2448 <ostapenko.andrey03@gmail.com>
2025-10-19 13:53:48 +0300

    Create Task model using Django ORM

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 from django_setup import *
-from lesson.models import User
+from lesson.models import Task, User
 
 # створення користувачів
-user1 = User.objects.create(name="Андрій", email="ostapenko.andrii@gmail.com", role="admin")
-user2 = User.objects.create(name="Кирило", email="grashanov.kirilo@gmail.com", role="user")
-user3 = User.objects.create(name="Вова", email="lebedenko.vova@gmail.com", role="user")
+# user1 = User.objects.create(name="Андрій", email="ostapenko.andrii@gmail.com", role="admin")
+# user2 = User.objects.create(name="Кирило", email="grashanov.kirilo@gmail.com", role="user")
+# user3 = User.objects.create(name="Вова", email="lebedenko.vova@gmail.com", role="user")
 
 print(User.objects.all())
 
-user2.role = "admin"
-user2.save()
+# user2.role = "admin"
+# user2.save()
 
 print(User.objects.get(id=2).role)
+
+task1 = Task.objects.create(
+    title="Зробити систему тасків",
+    description="Зробити систему управління тасками",
+    assigned_to=User.objects.get(id=2),
+)
+task2 = Task.objects.create(
+    title="Створити Django моделі для системи блогу та коментарів",
+    description="Створіть екземпляри постів та коментарів та забезпечте взаємодію, наприклад, додавання коментарів до посту чи редагування  посту.",
+    assigned_to=User.objects.get(id=3),
+)
+
+print(Task.objects.all())
+
+task1.status = "completed"
+task1.save()
+print(task1)
+
+task2.assigned_to = User.objects.get(id=1)
+task2.save()
+print(task2, task2.assigned_to)
